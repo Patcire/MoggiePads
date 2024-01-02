02f22e79ca5b3b6d56f919f4efaca91d42e114d9
Patricio <pcifrey501@g.educaand.es>
2024-01-02 02:07:25 +0100

    infinite scroll implemented

--- src/customHooks/useFetch.js
+++ src/customHooks/useFetch.js
@@ -1,19 +1,29 @@
 import {useEffect, useState} from "react";
 
-const useFetch = (url) => {
+const useFetch = (urlParameters) => {
 
+    const [page, setPage] = useState(0)
+    let url = ` https://api.thecatapi.com/v1/images/${urlParameters}&page=${page}`
+
+    // 9 es la longitud de los ids
+    if (urlParameters.length === 9) url =  ` https://api.thecatapi.com/v1/images/${urlParameters}`
     const [info, setInfo] = useState([])
+
+    const loadMoreInfo = () => {
+         setPage(page+1)
+    }
 
     useEffect(() => {
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                setInfo(data)
+                // todo --> implementar que no se repitan
+                setInfo((oldInfo) =>  [...oldInfo, ...data])
             })
             .catch(error => console.log(error))
-    }, []);
+    }, [url, page]);
 
-    return {info}
+    return {info, loadMoreInfo}
 
 }
 
--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,21 +1,51 @@
 import useFetch from "../customHooks/useFetch.js";
 import {token} from "../../token.js";
 import Card from "../components/Card.jsx";
+import {useEffect, useRef} from "react";
 
 
 const FeedPage = () => {
 
-    const { info} = useFetch(`https://api.thecatapi.com/v1/images/search?limit=12&has_breeds=1&${token}`)
+    const endPageRef = useRef(null)
+    const { info, loadMoreInfo} = useFetch(`search?limit=10${token}&has_breeds=1`)
     console.log(info)
+
+
+    const handleScroll = () => {
+
+        if (endPageRef.current && endPageRef.current.getBoundingClientRect().bottom <= window.innerHeight) {
+            loadMoreInfo()
+        }
+
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+
+    }, [info, handleScroll])
+
+    if (!info || info.length===0) {
+        return(
+            <h3 className={"cargando"}>Cargando...</h3>
+        )
+    }
+
     return(
+        <>
         <section className={"cards"}>
             {
                 info?.map((cat) => (
-                   <Card key={cat.id} cat={cat}></Card>
+                   <Card key={cat.id} cat={cat} loading="lazy"></Card>
                 ))
             }
         </section>
+        <div className={"endPageRef"} ref={endPageRef}></div>
+        </>
     )
+
 }
 
 export default FeedPage
