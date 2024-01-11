04d4df099f6569eb05ebcbcfacbb306a269c7e1e
Patricio <pcifrey501@g.educaand.es>
2024-01-11 08:28:04 +0100

    change data showed in cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({cat}) => {
                 <LikeButton cat={cat}></LikeButton>
                 <p className={"cards__card__info"}><strong>Raza</strong>: {cat.breeds[0].name}</p>
                 <p className={"cards__card__info"}><strong>Temperamento</strong>: {cat.breeds[0].temperament}</p>
-                <p className={"cards__card__info"}><strong>País de origen</strong>: {cat.breeds[0].country_code}</p>
+                <p className={"cards__card__info"}><strong>País de origen</strong>: {cat.breeds[0].origin}</p>
                 <p className={"cards__card__info"}><strong>Nivel de energía</strong>: {cat.breeds[0].energy_level}/5</p>
                 <p className={"cards__card__info"}><strong>Relación con perros</strong>: {cat.breeds[0].dog_friendly}/5</p>
             </article>
