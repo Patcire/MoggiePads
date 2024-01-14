205786bc7a7695cf40e12b8acba1850018982576
Patricio <pcifrey501@g.educaand.es>
2024-01-14 12:18:36 +0100

    some refactor on feedpage code

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -8,13 +8,13 @@ import {token} from "../../token.js";
 
 const FeedPage = () => {
 
+    let scrollPosition = window.scrollY
+    const endPageRef = useRef(null)
 
     const [page, setPage] = useState(0)
     const [url, setUrl] = useState(`https://api.thecatapi.com/v1/images/search?limit=16${token}&has_breeds=1&page=${page}`)
-    const endPageRef = useRef(null)
-    //const [info, setInfo]= useState()
     const [info, setInfo] = useState([])
-    let scrollPosition = window.scrollY
+
 
     const handleScroll = () => {
 
